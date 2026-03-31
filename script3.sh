54285519cbc97a217e520619cc57e1f416606179
tanishq0208-asus <tanishq.24bey10168@vitbhopal.ac.in>
2026-03-31 19:00:34 +0530

    Implement disk and permission auditor script
    Added a disk and permission auditing feature to check specified directories and VLC config directory.

--- script3.sh
+++ script3.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+# Script 3: Disk and Permission Auditor
+
+DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
+
 echo "Directory Audit Report"
-for dir in /etc /var/log /home /usr/bin /tmp
-do
-ls -ld $dir
-du -sh $dir
+echo "----------------------"
+
+for DIR in "${DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist"
+    fi
 done
+
+# VLC config directory check
+if [ -d "$HOME/.config/vlc" ]; then
+    PERM=$(ls -ld $HOME/.config/vlc | awk '{print $1, $3, $4}')
+    echo "VLC Config Directory => Permissions: $PERM"
+else
+    echo "VLC config directory not found"
+fi
